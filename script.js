0a8dc44092322281d9cce60513999aca135c6fe5
MUKUNDSINGHKUSHWAHA <96003278+MUKUNDSINGHKUSHWAHA@users.noreply.github.com>
2024-11-23 15:05:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const submit = document.querySelector("#email-btn");
+
+submit.addEventListener("click" , ()=>{
+    let error_img = document.getElementById('error-img');
+    // console.log(error_img);
+    error_img.style.display = "none";
+    let error_info = document.getElementById('error-info');
+    error_info.style.display = "none";
+
+    let emailField = document.getElementById('email-field');
+    let email = emailField.value;
+    let validity = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    if (validity) {
+        emailField.style.border = '2.5px solid #abd499';
+    }
+    else{
+        emailField.style.border = '2.5px solid #f96262';
+        error_info.style.display = "block";
+        error_img.style.display = "block";
+    }
+})
+
+
+    
